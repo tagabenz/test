81e1a7f860ee07d21c58ca16cc9cb99e3186c2bb
tagabenz <tagabenz@gmail.com>
2023-06-15 05:28:07 +0300

    add client app

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import json
 import asyncio, aiohttp
 
-from models import Base
+from models import *
 
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
-DATABASE_URL="postgresql+asyncpg://postgres:test@localhost/postgres"
-
-
-async def get_index_price():
+async def get_index_price(async_session):
     index_names=[
         "btc_usd",
         "eth_usd",
@@ -18,27 +15,28 @@ async def get_index_price():
     async with aiohttp.ClientSession() as session:
         for i in index_names:
             async with session.get(f"https://test.deribit.com/api/v2/public/get_index_price?index_name={i}") as response:
-                print(await response.json())
+                r = await response.json()
+                price=str(r['result']['index_price'])
+                if i == "btc_usd":object = Index(price=price,ticker='BTC')
+                else:object = Index(price=price,ticker='ETH')
+                await insert_object(async_session,object)
 
-
-async def init_models():
-    engine = create_async_engine(DATABASE_URL,echo=True)
+async def init_models(engine):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-    
 
-
-if __name__ == "__main__":
+def main():
+    engine = create_async_engine("postgresql+asyncpg://postgres:test@localhost/postgres",echo=True)
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(get_index_price, 'interval', seconds=3)
+    scheduler.add_job(init_models,args=[engine])
+    scheduler.add_job(get_index_price,'interval',args=[async_session],seconds=10)
     scheduler.start()
 
     try:asyncio.get_event_loop().run_forever()
     except (KeyboardInterrupt, SystemExit):pass
-    
-    
-    
 
-    
 
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -4,17 +4,20 @@ from datetime import datetime
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
-class Btc(Base):
-    __tablename__ = "btc"
-    price: Mapped[str]=mapped_column(primary_key=True)
+class Index(Base):
+    __tablename__ = "index"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ticker: Mapped[str]
+    price: Mapped[str]
     create_date: Mapped[datetime] = mapped_column(server_default=func.now())
 
 
-class Eth(Base):
-    __tablename__ = "eth"
-    price: Mapped[str]=mapped_column(primary_key=True)
-    create_date: Mapped[datetime] = mapped_column(server_default=func.now())
+async def insert_object(async_session,object):
+    async with async_session() as session:
+        async with session.begin():
+            session.add(object)
